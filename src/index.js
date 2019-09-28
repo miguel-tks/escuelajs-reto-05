0dc8e03a96330e02b783d0b6d535945981fb65d1
Miguel Hernandez <miguel.tks@gmail.com>
2019-09-28 11:30:28 -0500

    1er problema - terminado

--- src/index.js
+++ src/index.js
@@ -1,38 +1,51 @@
-const $app = document.getElementById('app');
-const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const $app = document.getElementById("app");
+const $observe = document.getElementById("observe");
+//const API = "https://rickandmortyapi.com/api/character/";
+//const API = "https://randomuse.me/api/";
+var API = "https://us-central1-escuelajs-api.cloudfunctions.net/characters";
+const $storage = window.localStorage;
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
+      debugger;
+      $storage.setItem("next_fetch", response.info.next);
       const characters = response.results;
-      let output = characters.map(character => {
-        return `
+      let output = characters
+        .map(character => {
+          return `
       <article class="Card">
         <img src="${character.image}" />
         <h2>${character.name}<span>${character.species}</span></h2>
       </article>
-    `
-      }).join('');
-      let newItem = document.createElement('section');
-      newItem.classList.add('Items');
+    `;
+        })
+        .join("");
+      let newItem = document.createElement("section");
+      newItem.classList.add("Items");
       newItem.innerHTML = output;
       $app.appendChild(newItem);
     })
     .catch(error => console.log(error));
-}
+};
 
 const loadData = () => {
-  getData(API);
-}
-
-const intersectionObserver = new IntersectionObserver(entries => {
-  if (entries[0].isIntersecting) {
-    loadData();
+  if ($storage.length > 0) {
+    API = $storage.getItem("next_fetch");
   }
-}, {
-  rootMargin: '0px 0px 100% 0px',
-});
+  getData(API);
+};
+
+const intersectionObserver = new IntersectionObserver(
+  entries => {
+    if (entries[0].isIntersecting) {
+      loadData();
+    }
+  },
+  {
+    rootMargin: "0px 0px 100% 0px"
+  }
+);
 
 intersectionObserver.observe($observe);
